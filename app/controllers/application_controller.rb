788ea53aac919fa82ba5cef6629208f0030831ec
Andygeek <andy.eulogio@gmail.com>
2021-09-25 13:42:47 -0500

    Add authentication method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,34 @@
 class ApplicationController < ActionController::API
+
+  # Authentication is necessary to use this api
+  before_action :authenticate!
+
+  private
+
+  def authenticate!
+    # Get header of Authorization
+    header = request.header["Authorization"]
+    
+    # Authorization = bearer xxxx, we will use only xxx
+    header = header.split(' ').last if header
+
+    begin
+      # Decoding token
+      @decoded = JsonWebToken.decode(header)
+
+      # Get user_id from token
+      @current_user = User.find(@decoded[:user_id])
+
+    rescue ActiveRecord::RecordNotFound => e
+
+      render json: { errors: e.message }, status: :unauthorized
+
+    rescue JWT::DecodeError => e
+
+      render json: { errors: e.message }, status: :unauthorized
+
+    end
+
+  end
+    
 end
