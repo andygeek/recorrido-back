167286ca14775b5c6eac0a5542e7b76aecf1b77d
Andygeek <andy.eulogio@gmail.com>
2021-09-25 14:16:20 -0500

    Improve actions in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,54 @@
 class UsersController < ApplicationController
 
+  # Signup does not require authentication
+  skip_before_action :authenticate!, only: [:signup]
+
+  # GET /users
+  def index
+    users = User.all
+    render json: users.as_json(json_options)
+  end
+
   # POST /users/singup
   def signup
-    @user = User.create(signup_params)
-    render json: @user, status: :ok
+    user = User.new(signup_params)
+    if user.save
+      render json: user.as_json(json_options)
+    else
+      render json: { status: :bad, error: user.errors.message }
+    end
   end
 
   # POST /users/login
   def login
-    @user = User.where(:username => login_params[:username], :password => login_params[:password]).first
-    render json: @user, status: :ok
+
+    @user = User.find_by(email: login_params[:email])
+
+    # Thanks to bycript
+    if @user&.authenticate(login_params[:password])
+      
+      # Token die in 24 hours
+      time = Time.now + 24.hours.to_i
+      # Encode the user_id in the token
+      token = JsonWebToken.encode({user_id: @user.id}, time)
+      
+      render json: { toke: token, exp: time.strftime( "%d-%m-%Y %H:%M"), email: @user.email }
+    else
+      render json: { error: "unauthorized" }, status: :forbidden    
+    end
   end
 
   private 
+  # TO-DO : Switch to active_model_serializers
+  def json_options
+    { except: %i[created_at updated_at password_digest] }
+  end
 
   def signup_params
-    params.require(:user).permit(:name, :username, :password)
+    params.require(:user).permit(:name, :email, :password)
   end
 
   def login_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :price_alerts
 
+  get "/users" => "users#index"
+
   # Search for price alerts by user id
   get "/price_alerts/find_by_userid/:id" => "price_alerts#find_by_userid"
 
