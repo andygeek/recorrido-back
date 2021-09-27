0ce3736e9f4bbe5c771957b0a85094b80f85ab51
Andygeek <andy.eulogio@gmail.com>
2021-09-27 17:14:34 -0500

    Add 5 minutes in schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,6 +23,6 @@ env :PATH, ENV['PATH']
 set :environment, "development"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-every 1.minute do
+every 5.minute do
   rake "recorrido_tasks:search_price"
 end
